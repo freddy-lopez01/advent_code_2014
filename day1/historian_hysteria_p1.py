c91b881f3669fe75a5a04f567cff24649328e745
Freddy Lopez <freddlopez01@gmail.com>
2024-12-01 16:20:35 -0800

    renamed files to be more descriptive of their functions

--- day1/historian_hysteria_p1.py
+++ day1/historian_hysteria_p1.py
@@ -1,4 +1,9 @@
 def sort_data(fname: str) -> tuple([]):
+    """
+    sort_data(fname) takes in a file name and reads the contents of the file and formats the two integers on each line into two lists: list1 and list2
+    It then process each number and removes any empty space and the new line characters if applicable and then converts the str into an int so mathmatical
+    operations can be performed using the data in later functions
+    """
     list1 = []
     list2 = []
     with open(fname, "r") as f:
@@ -8,20 +13,18 @@ def sort_data(fname: str) -> tuple([]):
             list1.append(int(split_line[0]))
             list2.append(int(split_line[1]))
 
-    # print(sorted(list1))
-    # print(sorted(list2))
-
     return sorted(list1), sorted(list2)
 
 
 def calculate_distance(l1: list, l2: list) -> int:
-    # print(l1, l2)
+    """
+    calculate_distance(l1, l2) takes in two lists and finds the difference between the two numbers in the same index of both lists. Then, to account for any possible negative evaluations, I take that diff
+    and perform a abs() operation to ensure a positive result
+    """
     total_distance = 0
     for num in range(len(l1)):
         diff = abs(l2[num] - l1[num])
-        # print(diff)
         total_distance += diff
-
     return total_distance
 
 
